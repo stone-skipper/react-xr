ce9d6f612e7c72d9c7aa502dd43af805c07cf18c
stone-skipper <stone.skipper.ee@gmail.com>
2023-12-08 18:34:18 +0900

    add transition

--- examples/src/demos/Hands.tsx
+++ examples/src/demos/Hands.tsx
@@ -1,147 +1,133 @@
-import { Canvas, useLoader } from '@react-three/fiber'
+import { Canvas, useLoader, useFrame } from '@react-three/fiber'
 import { Box, Environment, Plane, OrbitControls, Cylinder } from '@react-three/drei'
 import { Hands, XR, VRButton, Controllers, Interactive, RayGrab } from '@react-three/xr'
-import React, { ComponentProps } from 'react'
-import { useSpring, animated } from '@react-spring/three'
+import React, { ComponentProps, useEffect, useRef } from 'react'
+// import { useSpring, animated } from '@react-spring/three'
 import * as THREE from 'three' // Import THREE for constants
+import { useMotionValue, useMotionValueEvent, animate } from 'framer-motion'
+export default function () {
+  const [current, setCurrent] = React.useState(2)
+  const currentNum = useMotionValue(current)
+  const [pureNumber, setPureNumber] = React.useState(current)
 
-export function Button(props: ComponentProps<typeof Box>) {
-  const [hover, setHover] = React.useState(false)
-  const [color, setColor] = React.useState(0x123456)
+  useMotionValueEvent(currentNum, 'change', (latest) => {
+    console.log('currentNum changed to', latest)
+    setPureNumber(latest)
+  })
 
-  return (
-    <Interactive
-      onSelect={() => setColor((Math.random() * 0xffffff) | 0)}
-      // onHover={() => setHover(true)}
-      onBlur={() => setHover(false)}
-    >
-      <Box {...props} args={[0.4, 0.1, 0.1]} scale={hover ? 1.5 : 1}>
-        <meshStandardMaterial color={color} />
-      </Box>
-    </Interactive>
-  )
-}
+  useEffect(() => {
+    animate(currentNum, current, { duration: 1 })
 
-function getRandomInt(min = 0, max = 10) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
-}
+    // currentNum.set(current)
+  }, [current])
 
-const imageUrls = [
-  '/images/cat.jpg',
-  '/images/sky.jpg',
-  '/images/latte.png'
-  // ... add paths for all 10 images
-]
-
-export function Planes(props: ComponentProps<typeof Box>) {
-  const textures = useLoader(THREE.TextureLoader, imageUrls)
-
-  const [hover, setHover] = React.useState(false)
-  const [color, setColor] = React.useState(0xffffff)
-  const [gap, setGap] = React.useState(1)
-
-  function InteractionCylinder(props: ComponentProps<typeof Cylinder>) {
-    return (
-      <Interactive
-        onSelect={() => {
-          setColor((Math.random() * 0xffffff) | 0)
-          if (gap === 1) {
-            setGap(0.4)
-          } else {
-            setGap(1)
-          }
-        }}
-        onHover={() => setHover(true)}
-        onBlur={() => setHover(false)}
-      >
-        <Cylinder {...props} args={[1.5, 1.5, 5, 32]} scale={hover ? 1.5 : 1}>
-          <meshStandardMaterial wireframe={true} color={color} />
-        </Cylinder>
-      </Interactive>
-    )
+  function getRandomInt(min = 0, max = 10) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
   }
-  const planes = []
-  const numberOfPlanes = 20
-  const towardTop = 10
-  const maxRadius = 8
 
-  for (let j = 0; j < towardTop; j++) {
+  const imageUrls = [
+    '/images/cat.jpg',
+    '/images/sky.jpg',
+    '/images/latte.png'
+    // ... add paths for all 10 images
+  ]
+
+  function Planes(props: ComponentProps<typeof Box>) {
+    const textures = useLoader(THREE.TextureLoader, imageUrls)
+
+    const [hover, setHover] = React.useState(false)
+    const [color, setColor] = React.useState(0xffffff)
+    const [gap, setGap] = React.useState(1)
+
+    // function Grab(props: ComponentProps<typeof Box>) {
+    //   const [hover, setHover] = React.useState(false)
+    //   const [color, setColor] = React.useState(0x123456)
+    //   const [come, setCome] = React.useState(false)
+    //   const springs = useSpring({ position: come === true ? [0, 0.8, 0.2] : [0, 0.8, -2] })
+
+    //   React.useEffect(() => {}, [come])
+
+    //   return (
+    //     <Interactive>
+    //       <RayGrab
+    //         onSelectStart={() => {
+    //           setCome(true)
+    //         }}
+    //         onSelect={() => {
+    //           setCome(true)
+    //         }}
+    //         onSelectEnd={() => {
+    //           setCome(false)
+    //         }}
+    //       >
+    //         <Box {...props} args={[0.6, 2, 1]} position={props.position} rotation={props.rotation}>
+    //           <meshStandardMaterial wireframe={true} />
+    //         </Box>
+    //       </RayGrab>
+    //     </Interactive>
+    //   )
+    // }
+
+    const planes = []
+    const interactionBoxes = []
+    const numberOfPlanes = 20
+    const towardTop = 10
+    const maxRadius = 8
+
     for (let i = 0; i < numberOfPlanes; i++) {
-      for (let k = 2; k < maxRadius; k++) {
-        const angle = (i / numberOfPlanes) * Math.PI * 2 // Angle for each plane
-        const extraRotation = k % 2 === 1 ? Math.PI / numberOfPlanes : 0 // Extra rotation for odd layers based on radius size k
-        const x = Math.cos(angle + extraRotation) * k * gap // X position
-        const z = Math.sin(angle + extraRotation) * k * gap // Z position (assuming Y is up/down)
+      for (let j = 0; j < towardTop; j++) {
+        for (let k = 2; k < maxRadius; k++) {
+          const angle = (i / numberOfPlanes) * Math.PI * 2
+          const extraRotation = k % 2 === 1 ? Math.PI / numberOfPlanes : 0
+          let x = Math.cos(angle + extraRotation) * (k + 2 - currentNum.get()) * gap
+          let z = Math.sin(angle + extraRotation) * (k + 2 - currentNum.get()) * gap
 
-        // Select a random texture for each plane
-        const texture = textures[Math.floor(Math.random() * textures.length)]
+          const opacity = k < current ? k + 1 - currentNum.get() : 1 - (1 / (maxRadius - 2)) * (k + 2 - currentNum.get())
 
-        // Add the plane to the array with position and rotation
-        planes.push(
-          <Interactive>
-            <Plane
-              key={i}
-              position={[x, j * 0.5, z]}
-              rotation={[0, -angle + Math.PI / 2, 0]} // Adjust the rotation to face the center
-              args={[getRandomInt(3, 5) * 0.1, getRandomInt(3, 5) * 0.1]} // Size of the plane
-            >
-              <meshStandardMaterial
-                // @ts-ignore
-                map={texture} // Apply the texture
-                wireframe={false}
-                transparent={true}
-                opacity={1 - (1 / (maxRadius - 2)) * k} // Set the opacity to 0.5
-                color={color}
-                side={THREE.DoubleSide}
-              />
-            </Plane>
-          </Interactive>
-        )
+          // Select a random texture for each plane
+          const texture = textures[(i * j * k) % textures.length]
+
+          // Add the plane to the array with position and rotation
+          planes.push(
+            <Interactive>
+              <Plane
+                key={i}
+                visible={k < current - 1 ? false : true}
+                position={[x, j * 0.5, z]}
+                rotation={[0, -angle + Math.PI / 2, 0]} // Adjust the rotation to face the center
+                args={[0.4, 0.4]} // Size of the plane
+              >
+                <meshStandardMaterial
+                  // @ts-ignore
+                  map={texture} // Apply the texture
+                  wireframe={false}
+                  transparent={true}
+                  opacity={opacity}
+                  color={color}
+                  side={THREE.DoubleSide}
+                />
+              </Plane>
+            </Interactive>
+          )
+
+          // interactionBoxes.push(<Grab rotation={[0, -angle + Math.PI / 2, 0]} position={[grabX, 0.5, grabZ]} />)
+        }
       }
     }
+    return <>{planes}</>
   }
+
   return (
     <>
-      {planes}
-      {/* <InteractionCylinder /> */}
-    </>
-  )
-}
-
-export function Grab(props: ComponentProps<typeof Box>) {
-  const [hover, setHover] = React.useState(false)
-  const [color, setColor] = React.useState(0x123456)
-  const [come, setCome] = React.useState(false)
-  const springs = useSpring({ position: come === true ? [0, 0.8, 0.2] : [0, 0.8, -2] })
-
-  React.useEffect(() => {}, [come])
-
-  return (
-    <Interactive>
-      <RayGrab
-        onSelectStart={() => {
-          setCome(true)
-        }}
-        onSelect={() => {
-          setCome(true)
-        }}
-        onSelectEnd={() => {
-          setCome(false)
+      <div
+        onClick={() => {
+          setCurrent(current + 1)
         }}
       >
-        <Box {...props} args={[2, 2, 0.1]} position={[0, 0.8, -1]}>
-          <meshStandardMaterial wireframe={true} />
-        </Box>
-      </RayGrab>
-    </Interactive>
-  )
-}
-
-export default function () {
-  return (
-    <>
+        Click me
+      </div>
       <VRButton onError={(e) => console.error(e)} />
       <Canvas>
         <XR>
@@ -153,13 +139,9 @@ export default function () {
           // modelLeft="/hand-left.gltf"
           // modelRight="/hand-right.gltf"
           />
-          {/* <RayGrab>
-            <Button position={[0.5, 0.8, 0]} />
-          </RayGrab>
-          <RayGrab>
-            <Button position={[-0.5, 0.8, 0]} />
-          </RayGrab> */}
+
           <Planes />
+
           <OrbitControls />
           <Controllers
             /** Optional material props to pass to controllers' ray indicators */
